7fba72742c2e6821a253f8a2f2716c60ec72627f
quentinms <socialnetworks@quentin.ms>
2012-01-15 01:36:42 +0100

    Handling of "error" 304.
    Script works well. Need further testing though.

--- dumper.py
+++ dumper.py
@@ -19,8 +19,9 @@ def fetch(username, category, fullname):
     limit = 100
     url = 'http://www.reddit.com/user/%s/%s/' % (username, category)
     url = '%s.json?limit=%s&after=%s' % (url, limit, fullname)
-    #TODO change user-agent
-    #req = Request(url, {'User-agent': 'Reddit Post Recommender'})
+    req = Request(url)
+    req.add_header('User-Agent', 'Reddit Post Recommender contact:qms@kth.se')
+    
     return json.load(urlopen(url))
     
         
@@ -36,6 +37,9 @@ def fetch_all(username, category, most_recent_post):
     while True:
         page = fetch(username, category, next_post)
         start_time = time.time()
+        #print(page)
+        if('error' in page and page['error'] == 304):
+            raise(Exception('Error304'))
         next_post = page['data']['after']
         #The JSON data is
         # converted to a native python "dictionary" by the json
@@ -45,10 +49,11 @@ def fetch_all(username, category, most_recent_post):
             subreddit = post['data']['subreddit']
             post_id = post['data']['id']
             #If we already fetched this post, we should stop
-            #if(post_id == most_recent_post):
-                # TODO see how we should quit
-                #print('Already imported this!')
-                #break
+            if(post_id == most_recent_post):
+                # TODO: see how we should quit
+                print('Already imported this!')
+                next_post = None
+                break
             #We store the most recent post (i.e. first link of the first json retrieved)
             if(is_first_time):
                 most_recent_post = post_id
@@ -74,15 +79,18 @@ def fetch_all(username, category, most_recent_post):
         sleep((2000 - (time.time() - start_time)) / 1000)
 
         if (next_post == None):
-            #print("Done with: " + username + " " + category)
+            print("Done with: " + username + " " + category)
             outputFile.close()
-            output_username_file = open('temp_userlist.txt', 'a')
             
-            toWrite = username + ',' + category + ',' + most_recent_post+'\r\n'
+            output_username_file = open('temp_userlist.txt', 'a')
+            toWrite = username + ',' + category + ',' + str(most_recent_post) + '\r\n'
             output_username_file.write(toWrite)
             output_username_file.close()
             break
         
+#Clean temp files that may be there from a previous run.
+if os.path.exists("./temp_userlist.txt"):
+    os.remove("./temp_userlist.txt")      
 if(len(sys.argv) == 1 or (not sys.argv[1] == 'loop')):
     for line in open('userlist.txt'):
         # Unlike other programming languages, reading a line in python also
@@ -102,9 +110,9 @@ if(len(sys.argv) == 1 or (not sys.argv[1] == 'loop')):
             error_file.write(username + '\n')
             error_file.close()
             continue
-        except:
-            print('Unexpected error: %s with %s' % (sys.exc_info()[0], username))
-            continue
+#        except:
+#            print('Unexpected error: %s with %s' % (sys.exc_info()[0], username))
+#            continue
         
 else:  
     while True:
@@ -118,22 +126,28 @@ else:
             category = splitted_line[1]
             most_recent_post = splitted_line[2]
             
-            print(username+"/"+category+"/"+most_recent_post)
-            
             try:
                 fetch_all(username, category, most_recent_post)
                 
             except HTTPError as e:
-                # TODO: Handle errors more intelligent
-                # Currently skips username entirely if server returns error
+                # TODO: Handle errors more intelligently
                 print('Server responded with %d. Skipping %s.' % (e.code, username))
                 error_file = open('error_users.txt', 'a')
                 error_file.write(username + '\n')
                 error_file.close()
                 continue
-#            except:
-#                print('Unexpected error: %s with %s' % (sys.exc_info()[0], username))
-#                continue
+            except Exception as f:
+                if (f.args[0] == "Error304"): #That is, not really an error, but we should try the user again later
+                    print('Error 304: ' + username + " - " + category)
+                    output_username_file = open('temp_userlist.txt', 'a')
+                    toWrite = username + ',' + category + ',' + str(most_recent_post) + '\r\n'
+                    output_username_file.write(toWrite)
+                    output_username_file.close()
+                    sleep(2)
+                
+                else:
+                    print('Unexpected error: %s with %s' % (sys.exc_info()[0], username))
+                    continue
         os.remove("./userlist2.txt")
         os.rename("./temp_userlist.txt", "./userlist2.txt")
         print('---')
